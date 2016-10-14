20b6a5731fa6abacebe178f31342c8d71f4a5824
Fabien C. Y. Benureau <fabien.benureau+git@gmail.com>
2016-10-15 01:47:56 +0200

    Working avoid behavior, negative wheel speed

--- braitenberg/vehicle.py
+++ braitenberg/vehicle.py
@@ -139,7 +139,10 @@ class Wheel:
             for act in self.acts:
 
                 s += act
-            self.speed = 1.0*exp(5.0*s)/len(self.acts)
+            if s > 0:
+                self.speed = min( 100, 1.0*exp(5.0* s)/len(self.acts))
+            if s < 0:
+                self.speed = max(-100, -1.0*exp(5.0*-s)/len(self.acts))
             self.acts = []
 
 
