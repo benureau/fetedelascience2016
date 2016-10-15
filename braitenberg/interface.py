72a7a94ad6e18f1032e463d9053ccc610a7a2ad0
Fabien C. Y. Benureau <fabien.benureau+git@gmail.com>
2016-10-15 09:01:35 +0200

    Sidebar complete.

--- braitenberg/interface.py
+++ braitenberg/interface.py
@@ -20,11 +20,14 @@ class Interface:
         def slider_style(slider, pos, active=True):
             slider.setPosition(*pos)
             
-            slider.setSize(15, 150)
+            slider.setSize(16, 150)
             slider.setRange(-100, 100)
             slider.setColorBackground(color(250,105, 0))
             slider.setColorForeground(color(255,185, 0))
-            slider.setColorActive(color(255,185, 0))
+            if not active:
+                slider.setColorActive(color(255,185, 0))
+            else:
+                slider.setColorActive(color(255,225, 0))
             slider.setColorValue(color(0))
             slider.setColorLabel(color(0))
             
@@ -36,27 +39,27 @@ class Interface:
         
             #slider.setLock(not active)
         
-            self.lines.append((pos[0], pos[1]+150/2, pos[0] + 14, pos[1] + 150/2))                
+            self.lines.append((pos[0], pos[1]+150/2, pos[0] + 15, pos[1] + 150/2))                
 
         self.lw_slider = self.cp5.addSlider("Roue Gauche")
         self.lw_slider.setValue(self.vehicle.left_wheel.speed)
-        slider_style(self.lw_slider, (902, 500), active=False)
+        slider_style(self.lw_slider, (902, 550), active=False)
     
         self.rw_slider = self.cp5.addSlider("Roue Droite")
         self.rw_slider.setValue(self.vehicle.right_wheel.speed)
-        slider_style(self.rw_slider, (982, 500), active=False)
+        slider_style(self.rw_slider, (982, 550), active=False)
 
         self.linkmap = {'Capteur Gauche -> Roue Gauche': 2,
                         'Capteur Droit -> Roue Droite' : 1,
                         'Capteur Gauche -> Roue Droite': 3,
                         'Capteur Droite -> Roue Gauche': 0}
 
-        # for name, pos_y in zip(self.linkmap.keys(), [550, 580, 610, 640]):
-        #     w_slider = self.cp5.addSlider(name)
-        #     w_slider.setValue(self.vehicle.links[self.linkmap[name]].w)
-        #     slider_style(w_slider, (50, pos_y), active=True)
-        #     w_slider.setRange(-1.0, 1.0)
-        #     w_slider.addListener(self.callback)
+        for name, pos in zip(self.linkmap.keys(), [(850-8, 460-50), (1050-8, 460-50), (850-8, 320-100), (1050-8, 320-100)]):
+            w_slider = self.cp5.addSlider(name)
+            w_slider.setValue(self.vehicle.links[self.linkmap[name]].w)
+            slider_style(w_slider, pos, active=True)
+            w_slider.setRange(-1.0, 1.0)
+            w_slider.addListener(self.callback)
             
 
     def callback(self, slider):
@@ -80,9 +83,35 @@ class Interface:
         self.sidebar_vehicle.sensors[1].act = self.vehicle.sensors[1].act
         self.sidebar_vehicle.draw()
         
+        stroke(color(250,105, 0, 100))
+        fill(color(250,105, 0, 100))
+        strokeWeight(2)
+
+        line (910, 600, 910, 460)
+        ellipse(910, 460, 4, 4)
+        line (990, 600, 990, 460)
+        ellipse(990, 460, 4, 4)
+
+        ellipse(930, 420, 4, 4)
+        line (930, 420, 900, 420)
+        line (900, 420, 850, 460)
+
+        ellipse(970, 420, 4, 4)
+        line (970, 420, 1000, 420)
+        line (1000, 420, 1050, 460)
+
+        ellipse(956, 380, 4, 4)
+        line (956, 380, 1000, 380)
+        line (1000, 380, 1050, 320)
+
+        ellipse(944, 380, 4, 4)
+        line (944, 380, 900, 380)
+        line (900, 380, 850, 320)
+
+                
         self.cp5.draw()
         
-        stroke(50, 50, 50)
+        stroke(255, 0, 0, 150)
         strokeWeight(1)
         for ln in self.lines:
             line(*ln)
